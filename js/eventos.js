99448bb95e41ac04f87bd6b28d813d5f5e0237da
Jessica Arf <jessicalealarf8@gmail.com>
2022-08-15 20:56:28 -0300

    adicionada funcionalidade de reservar ingresso na página eventos

--- js/eventos.js
+++ js/eventos.js
@@ -1,6 +1,14 @@
-const url = "https://xp41-soundgarden-api.herokuapp.com/events";
-const evento = document.querySelector("tbody")
+const url = "https://xp41-soundgarden-api.herokuapp.com";
 const atracao = document.querySelector("#atracao")
+const btnopen = document.querySelector(".btnopen")
+const inputID = document.querySelector("#inputId")
+const modal = document.querySelector("#modal")
+const btn = document.querySelector("#btn-fecha")
+const twoBtn = document.querySelector("#twobtn-fecha")
+const nome = document.querySelector("#name")
+const email = document.querySelector("#email")
+const ticket = document.querySelector("#lotacao")
+const btnEnviar = document.querySelector("#btn-enviar")
 
 function arrumarData (data) {
     let date = data.split(""); // cortar a data para adicionar o / nos lugares corretos
@@ -15,7 +23,7 @@ function arrumarData (data) {
   };
 
   async function todosEventos () {
-    const response = await fetch(url);
+    const response = await fetch(`${url}/events`);
     console.log(response)
     const resposta = await response.json();
 
@@ -24,11 +32,54 @@ function arrumarData (data) {
     <h2>${prop.name} - ${arrumarData(prop.scheduled)}</h2>
     <h4>${prop.attractions}</h4>
     <p>${prop.description}</p>
-    <button class="btn btn-primary" onclick="abreModal('${
-      prop._id
-    }')" >reservar ingresso</button>
+    <button class="btn btn-primary" onclick="showModal('${prop._id}')" >reservar ingresso</button>
     </article>`;
   })
     }
 
     todosEventos();
+
+    async function showModal(id) {
+      modal.setAttribute("style", "display:flex");
+      inputID.value = id;
+      const resposta = await fetch(`${url}/events/${id}`);}
+
+      btn.addEventListener('click', function fechaModal () {
+        modal.setAttribute("style", "display:none");
+        nome.value = "";
+        email.value = "";
+        inputID.value ="";
+      });
+
+      twoBtn.addEventListener('click', function fecharModal () {
+        modal.setAttribute("style", "display:none");
+        nome.value = "";
+        email.value = "";
+        inputID.value ="";
+      });
+        
+      btnEnviar.addEventListener('click', async function criarReserva (e) {
+        e.preventDefault();
+
+        const reserva = {
+        "owner_name": nome.value,
+        "owner_email": email.value,
+        "number_tickets": lotacao.value,
+        "event_id":inputID.value
+        }
+
+        const response = await fetch(`${url}/bookings`, {
+        method:"POST",
+        body: JSON.stringify(reserva),
+        headers: {
+          "Content-type": "application/json",
+      },},)
+
+      const resultado = await response.json();
+      console.log(response)
+
+    if (response.status == 201) {
+     alert("Reserva efetuada com sucesso")
+     window.location.href = "admin.html"
+    }
+      })
